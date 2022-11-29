bcc83b1e66121a7544785f7e04490af374e2a86d
pashatagir <108672605+pashatagir@users.noreply.github.com>
2022-11-29 09:57:58 +0000

    Deploying to gh-pages from @ pashatagir/goit-js-hw-08@d021fa4bcf43a2ed33e6a3205673e4bb50042e41 🚀

--- 02-video.abf77bd4.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe");new Vimeo.Player(e).getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.abf77bd4.js.map
--- 02-video.b0156f06.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe");new Vimeo.Player(e).getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.b0156f06.js.map

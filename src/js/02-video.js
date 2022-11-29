d021fa4bcf43a2ed33e6a3205673e4bb50042e41
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-29 11:57:30 +0200

    add lib @vimeo/player and fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,11 @@
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));

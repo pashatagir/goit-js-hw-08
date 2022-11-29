3a009969d240cad9a90ed3e7c5fb0bf78579ba62
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-29 03:57:40 +0200

    fixed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+
+const createGalleryMarkup = createCardsImageMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createCardsImageMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>
+    `;
+    })
+    .join('');
+}

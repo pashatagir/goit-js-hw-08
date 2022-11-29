fc9c8e21d43a902952c40552d94fe6445af43d0f
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-29 16:33:01 +0200

    fixed task 3 but not finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 300));
+
+populateTextarea();
+
+const formData = {};
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedInput = localStorage.getItem('feedback-form-state');
+  const parseInput = JSON.parse(savedInput);
+  if (savedInput) {
+    refs.input.value = parseInput.email;
+    refs.textarea.value = parseInput.message;
+  }
+}

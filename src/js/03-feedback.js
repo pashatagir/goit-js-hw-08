1d7d2985088c106c425bf80dd7324cb02b83c37a
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-11-29 21:00:04 +0200

    finished task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,34 +6,40 @@ const refs = {
   textarea: document.querySelector('textarea'),
 };
 
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onInput, 500));
 
 populateTextarea();
 
-let formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-
 function onInput(evt) {
   formData[evt.target.name] = evt.target.value;
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  // чесно кажучи цей іфчик тут тому що не зміг збагнути як позбутися undefined,
-  // який вилазить при оновлені сторінки якщо заповний один з якихось інпутів
+
   if (refs.input.value === '' || refs.textarea.value === '') {
     return alert('Я що дарма кодив? Заповни будь ласка усі комірки!');
   }
+
   evt.currentTarget.reset();
+
   console.log(formData);
+
   formData = {};
-  localStorage.removeItem('feedback-form-state');
+
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function populateTextarea() {
-  const savedInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const savedInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
   if (savedInput) {
     refs.input.value = savedInput.email;
     refs.textarea.value = savedInput.message;
